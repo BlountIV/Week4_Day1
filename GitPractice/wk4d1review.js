8cb047629e757e7b78313d24f3aef61d9b80b152
BlountIV <iv.mosesblount@gmail.com>
2022-10-25 14:23:50 -0400

    Yell at Ninja Turtles and Methods, revisited

--- GitPractice/wk4d1review.js
+++ GitPractice/wk4d1review.js
@@ -7,68 +7,75 @@
 // Get Even
 ////////////////////////////////
 
-const getEven = () => {
-    for(let i = 0; i <= 200; i++);
-    if(i % 2 == 0 && i !== 0) {
-        console.log(i)
-    }
-}
-getEven();
-////////////////////////////////
-// Fizz Buzz
-////////////////////////////////
+// const getEven = () => {
+//     for(let i = 0; i <= 200; i++);
+//     if(i % 2 == 0 && i !== 0) {
+//         console.log(i)
+//     }
+// }
+// getEven();
+// ////////////////////////////////
+// // Fizz Buzz
+// ////////////////////////////////
 
-const getMyNumbers = () =>{
-    let num;
-        for(let i=1; i<101; i++){
-            num=i;
+// const getMyNumbers = () =>{
+//     let num;
+//         for(let i=1; i<101; i++){
+//             num=i;
         
-        if(num%3===0 && num%5===0){
-            console.log(`FizzBuzz`);
-        }
-        else if(num%3===0){
-            console.log(`Fizz`);
-        }
-        else if(num%5===0){
-            console.log(`Buzz`);
-        }
-        else {
-            console.log(num);
-        }
-    }
-}
-getMyNumbers();
+//         if(num%3===0 && num%5===0){
+//             console.log(`FizzBuzz`);
+//         }
+//         else if(num%3===0){
+//             console.log(`Fizz`);
+//         }
+//         else if(num%5===0){
+//             console.log(`Buzz`);
+//         }
+//         else {
+//             console.log(num);
+//         }
+//     }
+// }
+// getMyNumbers();
 
 ////////////////////////////////
 // Wild Wild Life
 ////////////////////////////////
 
-const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
-const sharky = ["Sharky", "shark", 20, "Left Coast"]
-const plantee = ["Plantee", "plant",  5000 , "Mordor"]
-const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
-const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+// const sharky = ["Sharky", "shark", 20, "Left Coast"]
+// const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+// const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+// const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
 
-plantee[2] += 1; 
-console.log(plantee);
+// plantee[2] += 1; 
+// console.log(plantee);
 
-wolfy[3] = 'Gotham City'
+// wolfy[3] = 'Gotham City'
 
-dart[3] += 'Hawkins' 
-console.log(dart)
+// dart[3] += 'Hawkins' 
+// console.log(dart)
 
-wolfy.shift(); 
-wolfy,unshift('Gameboy');
-console.log(wolfy);
+// wolfy.shift(); 
+// wolfy,unshift('Gameboy');
+// console.log(wolfy);
+
 ////////////////////////////////
 // Yell at the Ninja Turtles
 ////////////////////////////////
 
+// const ninjaTurtles = ['Donatello', 'Leonardo', 'Raphael', 'Michaelangelo'];
+// for (const element of ninjaTurtles) {
+//     console.log(element.toUpperCase());
+// }
 
 ////////////////////////////////
 // Methods, Revisited
 ////////////////////////////////
+// const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
 
+// console.log(`favMovies.indexOf("Titanic") is ${favMovies.indexOf('Titanic')}`);
 
 ////////////////////////////////
 // Where is Waldo
